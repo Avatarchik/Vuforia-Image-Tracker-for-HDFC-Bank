a33567faea6466d30e6c0e5ea81d53e20af327e1
Lintzmaia <Thiagolintzmaia@hotmail.com>
2016-06-14 03:47:33 -0400

    Line Scanner Effect

---
+++ Assets/Scripts/LineAnimationController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineAnimationController : MonoBehaviour {
+	Animator Anim;
+	// Use this for initialization
+	void Start () {
+		Anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Screen.orientation == ScreenOrientation.Landscape) {
+			GetComponent<RectTransform> ().rotation = Quaternion.Euler (0, 0, 90);
+			Anim.SetBool ("Landscape", true);
+			Anim.SetBool ("Portrait", false);
+		}
+		if (Screen.orientation == ScreenOrientation.Portrait) {
+			GetComponent<RectTransform> ().rotation = Quaternion.Euler (0, 0, 0);
+			Anim.SetBool ("Landscape", false);
+			Anim.SetBool ("Portrait", true);
+		}
+	}
+}
---
+++ Assets/Scripts/LineController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Target.y = transform.position.y;
+		Target.z = transform.position.z;
+		Target.x = RightEdge;
+	}
+	public float RightEdge;
+	public float LeftEdge;
+	Vector3 Target;
+	// Update is called once per frame
+	void Update () {
+		if (transform.position == Target) {
+			if (Target.x == RightEdge) {
+				Target.x = LeftEdge;
+			} else if (Target.x == LeftEdge) {
+				Target.x = RightEdge;
+			}
+		}
+		transform.position = Vector3.MoveTowards (transform.position, Target, 2);
+
+	}
+}
